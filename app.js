96b1469f1ad838e24309fe4516005a3430f2b546
saunmanoj888 <saunmanoj628@gmail.com>
2020-04-12 00:31:49 +0530

    change questions

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const correctAnswers = ['A', 'A', 'A', 'A'];
+const correctAnswers = ['B', 'B', 'A', 'A'];
 const form = document.querySelector('.quiz-form');
 const result = document.querySelector('.result');
 
